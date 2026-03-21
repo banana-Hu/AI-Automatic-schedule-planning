ce5727beb3a7e505650e1f83de92536b4cf93271
banana-Hu <3473972674@qq.com>
2026-03-22 00:48:19 +0800

    Fix: Remove afterEvaluate to avoid lifecycle crash, use configureEach for Java 17

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -18,15 +18,13 @@ subprojects {
 }
 
 subprojects {
-    afterEvaluate {
-        tasks.withType<org.gradle.api.tasks.compile.JavaCompile>().configureEach {
-            sourceCompatibility = "17"
-            targetCompatibility = "17"
-        }
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-            kotlinOptions {
-                jvmTarget = "17"
-            }
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "17"
         }
     }
 }
